0ea3d281bc5fd7c649f3929019dc82493a5ba5c5
unknown <lehuy722@gmail.com>
2017-11-25 22:40:48 +0700

    lehuy

---
+++ app/Jobs/SendWellcomeEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Mail\WellcomeEmail;
+use App\User;
+use Mail;
+
+class SendWellcomeEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    protected $user;
+    
+    public function __construct($user)
+    {
+        //
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+        sleep(60);
+        echo "Start send Email";
+        $email = new WellcomeEmail($this->user);
+        Mail::to($this->user->email)->send($email);
+
+        echo "End";
+    }
+}
